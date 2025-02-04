5f4cefe7faa0788bd3a8862650d12a2bf61fa592
KGUYODO <kevin.guyodo@ynov.com>
2025-02-04 21:13:32 +0100

    authentication done, check the token for an exemple of API route

--- start/routes.ts
+++ start/routes.ts
@@ -1,16 +1,34 @@
 import router from '@adonisjs/core/services/router'
 import User from '#models/user'
 import { middleware } from './kernel.js'
+import { Exception } from '@adonisjs/core/exceptions'
+import hash from '@adonisjs/core/services/hash'
 
-router.get('/', () => {
-  return { "name": "Hello World young frog" }
-}).use(middleware.auth({
-  guards: ['api']
-}))
+router
+  .get('/', () => {
+    return { name: 'Hello World young frog' }
+  })
+  .use(
+    middleware.auth({
+      guards: ['api'],
+    })
+  )
 
-router.post('users/:id/tokens', async ({ params }) => {
-  const user = await User.findOrFail(params.id)
-  const token = await User.accessTokens.create(user, ['*'], {expiresIn: "1 day"})
+router.post('/login', async ({ request }) => {
+  const requestBody = request.body() as User
+  const user = await User.query().where('email', requestBody.email).firstOrFail()
+
+  if (!user) {
+    throw new Exception('User does not exist', { status: 404 })
+  }
+
+  // Check that the basic password and the password of the user who wants to connect are the same
+  const isValidPassword = await hash.verify(user.password, requestBody.password)
+  if (!isValidPassword) {
+    throw new Exception('User does not exist', { status: 404 })
+  }
+
+  const token = await User.accessTokens.create(user, ['*'], { expiresIn: '1 day' })
 
   return {
     type: 'bearer',
@@ -18,12 +36,12 @@ router.post('users/:id/tokens', async ({ params }) => {
   }
 })
 
-router.post('users', async ({request}) => {
-  const { email, fullName, password } = request.body as unknown as User
+router.post('/users', async ({ request }) => {
+  const { email, fullName, password } = request.body() as User
   const test = await User.create({
     email,
     fullName,
-    password
+    password,
   })
 
   return test.serialize()
