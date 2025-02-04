746b51fadfb0eea1b8af15cca12940be292233e9
kevin <kevin.guyodo@ynov.com>
2025-02-04 19:25:45 +0100

    create user creation/login route

--- start/routes.ts
+++ start/routes.ts
@@ -1,16 +1,30 @@
-/*
-|--------------------------------------------------------------------------
-| Routes file
-|--------------------------------------------------------------------------
-|
-| The routes file is used for defining the HTTP routes.
-|
-*/
-
 import router from '@adonisjs/core/services/router'
+import User from '#models/user'
+import { middleware } from './kernel.js'
 
-router.get('/', async () => {
+router.get('/', () => {
+  return { "name": "Hello World young frog" }
+}).use(middleware.auth({
+  guards: ['api']
+}))
+
+router.post('users/:id/tokens', async ({ params }) => {
+  const user = await User.findOrFail(params.id)
+  const token = await User.accessTokens.create(user, ['*'], {expiresIn: "1 day"})
+
   return {
-    hello: 'world',
+    type: 'bearer',
+    value: token.value!.release(),
   }
+})
+
+router.post('users', async ({request}) => {
+  const { email, fullName, password } = request.body as unknown as User
+  const test = await User.create({
+    email,
+    fullName,
+    password
+  })
+
+  return test.serialize()
 })
