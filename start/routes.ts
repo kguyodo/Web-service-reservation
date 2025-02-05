f719457713ab5b2d9aecb99ed8d542ffeb505d5e
KGUYODO <kevin.guyodo@ynov.com>
2025-02-05 08:20:40 +0100

    create group routes

--- start/routes.ts
+++ start/routes.ts
@@ -14,35 +14,39 @@ router
     })
   )
 
-router.post('/login', async ({ request }) => {
-  const requestBody = request.body() as User
-  const user = await User.query().where('email', requestBody.email).firstOrFail()
+router
+  .group(() => {
+    router.post('/login', async ({ request }) => {
+      const requestBody = request.body() as User
+      const user = await User.query().where('email', requestBody.email).firstOrFail()
 
-  if (!user) {
-    throw new Exception('User does not exist', { status: 404 })
-  }
+      if (!user) {
+        throw new Exception('User does not exist', { status: 404 })
+      }
 
-  // Check that the basic password and the password of the user who wants to connect are the same
-  const isValidPassword = await hash.verify(user.password, requestBody.password)
-  if (!isValidPassword) {
-    throw new Exception('User does not exist', { status: 404 })
-  }
+      // Check that the basic password and the password of the user who wants to connect are the same
+      const isValidPassword = await hash.verify(user.password, requestBody.password)
+      if (!isValidPassword) {
+        throw new Exception('User does not exist', { status: 404 })
+      }
 
-  const token = await User.accessTokens.create(user, ['*'], { expiresIn: '1 day' })
+      const token = await User.accessTokens.create(user, ['*'], { expiresIn: '1 day' })
 
-  return {
-    type: 'bearer',
-    value: token.value!.release(),
-  }
-})
+      return {
+        type: 'bearer',
+        value: token.value!.release(),
+      }
+    })
 
-router.post('/users', async ({ request }) => {
-  const { email, fullName, password } = request.body() as User
-  const newUser = await User.create({
-    email,
-    fullName,
-    password,
+    router.post('/register', async ({ request }) => {
+      const { email, fullName, password } = request.body() as User
+      const newUser = await User.create({
+        email,
+        fullName,
+        password,
+      })
+
+      return newUser.serialize()
+    })
   })
-
-  return newUser.serialize()
-})
+  .prefix('/users')
