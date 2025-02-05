358e37e114fc8f001d1dde096d581170ee870a80
KGUYODO <kevin.guyodo@ynov.com>
2025-02-05 14:20:49 +0100

    add swagger documentation customers

--- app/controllers/customers_controller.ts
+++ app/controllers/customers_controller.ts
@@ -4,6 +4,13 @@ import Customer from '#models/customer'
 import { customerValidator } from '#validators/customer'
 
 export default class CustomersController {
+  /**
+   * @getCustomers
+   * @param {HttpContext} - HTTP context object containing request
+   * @description Returns all customers from PostgreSQL database
+   * @responseBody 200 - Array of Customer objects - @type(Customer[])
+   * @responseBody 500 - Internal Server Error
+   */
   async getCustomers({ response }: HttpContext) {
     try {
       const customers = await Customer.all()
@@ -13,6 +20,14 @@ export default class CustomersController {
     }
   }
 
+  /**
+   * @getOneCustomer
+   * @param {HttpContext} - HTTP context object containing request and response
+   * @description Returns one customer with specific customer id (inside URL) from PostgreSQL database
+   * @responseBody 200 - Customer object - @type(Customer)
+   * @responseBody 404 - Customer not found
+   * @responseBody 500 - Internal Server Error
+   */
   async getOneCustomer({ request, response }: HttpContext) {
     try {
       const customer = await Customer.find(request.param('id'))
@@ -26,12 +41,31 @@ export default class CustomersController {
     }
   }
 
+  /**
+   * @postCustomer
+   * @param {HttpContext} - HTTP context object containing request and response
+   * @description Create new Customer in database
+   * @requestBody Customer - Customer data to be created - @type(Customer) @required
+   * @responseBody 200 - Newly created Customer - @type(Customer)
+   * @responseBody 400 - Bad Request
+   * @responseBody 500 - Internal Server Error
+   */
   async postCustomer({ request, response }: HttpContext) {
     const customerData = request.body() as Customer
     const newCustomer = await Customer.create(customerData)
     return response.json(newCustomer)
   }
 
+  /**
+   * @postCustomer
+   * @param {HttpContext} - HTTP context object containing request and response
+   * @description Update Customer in database
+   * @requestBody Customer - Customer data to be modified - @type(Customer) @required
+   * @responseBody 200 - Updated Customer - @type(Customer)
+   * @responseBody 400 - Bad Request
+   * @responseBody 404 - Customer not found
+   * @responseBody 500 - Internal Server Error
+   */
   async putCustomer({ request, response }: HttpContext) {
     const customer = await Customer.find(request.param('id'))
     if (!customer) {
