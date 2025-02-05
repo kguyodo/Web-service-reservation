c644736cc58f377d154ac3256eb985a6a83d3b7a
Nathan Schneider <nathan.schneider4505@gmail.com>
2025-02-05 09:12:28 +0100

    WIP on features/experience-handling

--- app/controllers/experiences_controller.ts
+++ app/controllers/experiences_controller.ts
@@ -2,9 +2,21 @@ import type { HttpContext } from '@adonisjs/core/http'
 
 export default class ExperiencesController {
 
+    getExperiences({request, response}: HttpContext) {
+
+        console.log('GET Experience')
+        return response.status(200)
+    }
+
     createExperience({request, response}: HttpContext) {
 
-        console.log(request.body);
-        return response.status(201);
+        console.log(request.body)
+        return response.status(201)
+    }
+
+    deleteExperience({request, response}: HttpContext) {
+
+        console.log('DELETE Experience')
+        return response.status(204)
     }
 }
--- start/routes.ts
+++ start/routes.ts
@@ -1,18 +1,8 @@
-/*
-|--------------------------------------------------------------------------
-| Routes file
-|--------------------------------------------------------------------------
-|
-| The routes file is used for defining the HTTP routes.
-|
-*/
-
-const MailController = () => import('#controllers/mail_controller')
 import router from '@adonisjs/core/services/router'
-import User from '#models/user'
 import { middleware } from './kernel.js'
-import { Exception } from '@adonisjs/core/exceptions'
-import hash from '@adonisjs/core/services/hash'
+
+const ExperiencesController = () => import('#controllers/experiences_controller')
+const UsersController = () => import('#controllers/users_controller')
 
 router
   .get('/', () => {
@@ -24,41 +14,15 @@ router
     })
   )
 
-router
-  .group(() => {
-    router.post('/login', async ({ request }) => {
-      const requestBody = request.body() as User
-      const user = await User.query().where('email', requestBody.email).firstOrFail()
+router.group(() => {
+  router.post('/login', [UsersController, 'login'])
+  router.post('/register', [UsersController, 'register'])
+})
+.prefix('/users')
 
-      if (!user) {
-        throw new Exception('User does not exist', { status: 404 })
-      }
-
-      // Check that the basic password and the password of the user who wants to connect are the same
-      const isValidPassword = await hash.verify(user.password, requestBody.password)
-      if (!isValidPassword) {
-        throw new Exception('User does not exist', { status: 404 })
-      }
-
-      const token = await User.accessTokens.create(user, ['*'], { expiresIn: '1 day' })
-
-      return {
-        type: 'bearer',
-        value: token.value!.release(),
-      }
-    })
-
-    router.post('/register', async ({ request }) => {
-      const { email, fullName, password } = request.body() as User
-      const newUser = await User.create({
-        email,
-        fullName,
-        password,
-      })
-
-      return newUser.serialize()
-    })
-  })
-  .prefix('/users')
-
-router.get('/send-mail', [MailController, 'sendEmail']).use(middleware.auth({ guards: ['api'] }))
+router.group(() => {
+  router.get('/', [ExperiencesController, 'getExperiences'])
+  router.post('/', [ExperiencesController, 'createExperience'])
+  router.delete('/', [ExperiencesController, 'deleteExperience'])
+})
+.prefix('/experiences')
